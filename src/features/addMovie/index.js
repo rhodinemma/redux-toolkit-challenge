280b7a4cadd132739febe0b7d84e22110185396b
rhodinemma <rhodinemma10@gmail.com>
2022-11-22 13:43:42 +0300

    ch(structure-app)

---
+++ src/features/addMovie/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddMovie = () => {
+  return (
+    <div>
+      <h2>AddMovie</h2>
+    </div>
+  );
+};
+
+export default AddMovie;
---
+++ src/features/watchListMovies/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WatchListMovies = () => {
+  return <div>WatchListMovies</div>;
+};
+
+export default WatchListMovies;
