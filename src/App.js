4a9d2c82993f07c01fd4ccd3e13348405445cd48
rhodinemma <rhodinemma10@gmail.com>
2022-11-22 14:09:25 +0300

    ft(configure-routes)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AddMovie from "./features/addMovie";
+import WatchedMovies from "./features/watchedMovies";
+import WatchListMovies from "./features/watchListMovies";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Move Watchlist</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WatchListMovies />}></Route>
+        <Route path="/add" element={<AddMovie />}></Route>
+        <Route path="/watched" element={<WatchedMovies />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
